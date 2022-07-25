1c2e7cfe775d130014fb81228812fe38121ae077
Yechan <77843815+yechanball@users.noreply.github.com>
2022-07-25 23:19:47 +0900

    feat: Add BOJ-1929-Java

---
+++ baekjoon/Main_1929_소수구하기.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1929_소수구하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int[] num = new int[N+1];
+		int cnt = 0;
+		
+		num[1] = 1;
+		for(int i = 1; i <= N; i++) {
+			if(num[i] == 0) {
+				cnt = 2;
+				while(i*cnt <= N) {
+					num[i*cnt] = 1;
+					cnt++;
+				}
+			}
+		}
+		
+		for(int i = M; i <= N; i++) {
+			if(num[i] == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
