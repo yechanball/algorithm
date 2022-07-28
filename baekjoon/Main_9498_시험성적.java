cbf0ae4c8313fae16d4d39eb2128194d5e22957a
Yechan <77843815+yechanball@users.noreply.github.com>
2022-07-28 22:45:21 +0900

    feat: Add BOJ-9498-Java

---
+++ baekjoon/Main_9498_시험성적.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_9498_시험성적 {
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+        char grade;
+
+        if(score>=90 && score<=100){
+            grade = 'A';
+        }else if(score>=80 && score<=89){
+            grade = 'B';
+        }else if(score>=70 && score<=79){
+            grade = 'C';
+        }else if(score>=60 && score<=69){
+            grade = 'D';
+        }else{
+            grade = 'F';
+        }
+
+        System.out.println(grade);
+		sc.close();
+	}
+}
