6b74d0009b1d0fbaed1644c169c219df0ddfe4f4
Yechan <77843815+yechanball@users.noreply.github.com>
2022-07-30 16:34:54 +0900

    feat: Add BOJ-2562-Java

---
+++ baekjoon/Main_2562_최댓값.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2562_최댓값 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = 9;
+		int max = Integer.MIN_VALUE;
+		int maxIdx = 0;
+		int num = 0;
+		int idx = 1;
+		
+		while(idx <= n) {
+			num = Integer.parseInt(in.readLine());
+			if(num > max) {
+				max = num;
+				maxIdx = idx;
+			}
+			idx++;
+		}
+		
+		System.out.println(max);
+		System.out.println(maxIdx);		
+	}
+}
