dd0d1ec25994307ca47b23472a827436be2e0013
Yechan <77843815+yechanball@users.noreply.github.com>
2022-07-31 23:42:49 +0900

    feat: Add BOJ-10818-Java

---
+++ baekjoon/Main_10818_최소최대.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10818_최소최대 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		String str = in.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		int num = 0;
+		
+		for (int i = 0; i < N; i++) {
+			num = Integer.parseInt(st.nextToken());
+			if(num > max) {
+				max = num;
+			}if(num < min) {
+				min = num;
+			}
+		}
+		
+		System.out.println(min+" "+max);
+	}
+}
