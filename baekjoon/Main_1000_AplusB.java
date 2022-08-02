8f0b1cd92d4e1d2842313fe40af5ed19762404b0
공예찬 <rhddpcks9814@naver.com>
2022-08-02 16:39:18 +0900

    feat: Add BOJ-1000-Java

---
+++ baekjoon/Main_1000_AplusB.java
@@ -0,0 +1,14 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1000_AplusB {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		System.out.print(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()));
+	}
+}
