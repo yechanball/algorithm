f496d46d26eae46dd0a1b88fa802f4b73f8f3476
공예찬 <rhddpcks9814@naver.com>
2022-08-05 17:48:14 +0900

    refactor: Refactor BOJ-2493-Java

--- baekjoon/Main_2493_탑.java
+++ baekjoon/Main_2493_탑.java
@@ -12,26 +12,29 @@ public class Main_2493_탑 {
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(in.readLine());
 		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-		int[] tower = new int[N];
+		Stack<Integer> tower = new Stack<>();
 		for (int i = 0; i < N; i++) {
-			tower[i] = Integer.parseInt(st.nextToken());
+			tower.push(Integer.parseInt(st.nextToken()));
 		}
-		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> contact = new Stack<>();
 		boolean isContact = false;
-		for (int i = N-1; i >= 0; i--) {
+		int nowTower;
+		while (!tower.empty()) {
 			isContact = false;
-			for (int j = i-1; j >= 0; j--) {
-				if(tower[j] > tower[i]) {
+			nowTower = tower.pop();
+			
+			for (int j = tower.size()-1; j >= 0; j--) {
+				if(tower.elementAt(j) > nowTower) {
 					isContact = true;
-					stack.push(j+1);
+					contact.push(j+1);
 					break;
 				}
 			}
-			if(!isContact) stack.push(0);
+			if(!isContact) contact.push(0);
 		}
-		int size = stack.size();
+		int size = contact.size();
 		for (int i = 0; i < size; i++) {
-			sb.append(stack.pop()).append(" ");
+			sb.append(contact.pop()).append(" ");
 		}
 		System.out.print(sb);
 	}
