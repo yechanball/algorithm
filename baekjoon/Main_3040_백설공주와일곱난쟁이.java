c83c276fcab6a0f937b42bbbc493a98e6e0d71e0
yechan <rhddpcks9814@naver.com>
2022-08-11 21:16:11 +0900

    refactor: Refactor BOJ-3040-Java

--- baekjoon/Main_3040_백설공주와일곱난쟁이.java
+++ baekjoon/Main_3040_백설공주와일곱난쟁이.java
@@ -7,36 +7,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main_3040_백설공주와일곱난쟁이 {
+	static StringBuilder sb = new StringBuilder();
 	static int[] miniMe = new int[9]; // 난쟁이 모자에 적힌 수 배열
 	static int[] realMiniMe = new int[7]; // 리얼 일곱 난쟁이
 	
-	public static void combi(int cnt, int start, int sum, int flag) throws IOException {
+	public static void combi(int cnt, int start, int sum, int flag) {
 		if(cnt == 7) {
 			if(sum == 100) { // 일곱명 합이 100이면 출력후 종료
-				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-				StringBuilder sb = new StringBuilder();
 				for (int i = 0; i < 7; i++) {
 					sb.append(realMiniMe[i]).append("\n");
 				}
-				out.write(sb.toString());
-				out.flush();
-				out.close();
 				return;
 			}
 		}else {
 			for (int i = start; i < 9; i++) {
 				if( (flag & 1<<i) != 0 ) continue; // 이미 마스킹되어 있으면 건너뛰기
 				realMiniMe[cnt] = miniMe[i];
-				combi(cnt+1, start+1, sum+miniMe[i], flag | 1<<i);
+				combi(cnt+1, i+1, sum+miniMe[i], flag | 1<<i);
 			}
 		}
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		for (int i = 0; i < 9; i++) {
 			miniMe[i] = Integer.parseInt(in.readLine());
 		}
 		combi(0, 0, 0, 0); // 일곱 난쟁이 찾기
+		out.write(sb.toString());
+		out.flush();
+		out.close();
 	}
 }
