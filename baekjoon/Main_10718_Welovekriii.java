83181f5defb84f4d6efdec93254702190193f7c4
공예찬 <rhddpcks9814@naver.com>
2022-08-12 17:45:37 +0900

    feat: Add BOJ-10718-Java

---
+++ baekjoon/Main_10718_Welovekriii.java
@@ -0,0 +1,8 @@
+package algorithm.baekjoon;
+
+public class Main_10718_Welovekriii {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.print("강한친구 대한육군");
+	}
+}
