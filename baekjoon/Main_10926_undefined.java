2db204bcf7ff8084fd14d5a6b3feb0e68443409d
yechan <rhddpcks9814@naver.com>
2022-08-15 03:36:47 +0900

    feat: Add BOJ-10926-Java

---
+++ baekjoon/Main_10926_undefined.java
@@ -0,0 +1,19 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main_10926_undefined {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		sb.append(in.readLine()).append("??!");
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+}
