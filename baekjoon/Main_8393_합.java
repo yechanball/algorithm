b0b8eb2dc0270033e61e3a699d77b19aa96fdde7
yechan <rhddpcks9814@naver.com>
2022-08-15 03:20:41 +0900

    feat: Add BOJ-8393-Java

---
+++ baekjoon/Main_8393_합.java
@@ -0,0 +1,20 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_8393_합 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(in.readLine());
+		int sum = N*(N+1)/2;
+		out.write(Integer.toString(sum));
+		out.flush();
+		out.close();
+	}
+}
