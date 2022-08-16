b633fc185875fab1ffc1377a4b00779436c8ea1f
yechan <rhddpcks9814@naver.com>
2022-08-16 13:45:57 +0900

    refactor: Refactor BOJ-2839-Java

--- baekjoon/Main_2839_설탕배달.java
+++ baekjoon/Main_2839_설탕배달.java
@@ -1,31 +1,34 @@
 package algorithm.baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main_2839_설탕배달 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(in.readLine());
-		int three = 0, five = 0;
-		int min = Integer.MAX_VALUE;
-		int num = 0;
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		while(five*5 <= N) {
-			if( (N - (five*5))%3 == 0 ) {
-				three = (N - (five*5))/3;
-				num = five + three;
-				if(num < min) {
-					min = num;
-				}
+		int N = Integer.parseInt(in.readLine());
+		int three = 0, five = N/5; // 3킬로 봉지수와 5킬로 봉지수
+		int min = Integer.MAX_VALUE; // 봉지 최소 개수
+		int remain;
+		
+		while(five*5 >= 0) {
+			remain = N - (five*5); // 5킬로 봉지를 제외하고 나머지
+			if( remain%3 == 0 ) { // 나머지가 3킬로 봉지로 채울 수 있는지 확인
+				three = remain/3;
+				min = five + three; // 5킬로 봉지와 3킬로 봉지의 합이 최솟값
+				break;
 			}
-			five++;
+			five--;
 		}
 		
-		if(min == Integer.MAX_VALUE)
-			System.out.println("-1");
-		else
-			System.out.println(min);
+		if(min == Integer.MAX_VALUE) out.write("-1");
+		else out.write(Integer.toString(min));
+		out.flush();
+		out.close();
 	}
 }
