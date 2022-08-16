baad828f8ad27d48f4dcf36c5c2b3cae3e2c778d
yechan <rhddpcks9814@naver.com>
2022-08-16 09:18:23 +0900

    feat: Add BOJ-15733-Java

---
+++ baekjoon/Main_15733_나는누구인가.java
@@ -0,0 +1,14 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main_15733_나는누구인가 {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		out.write("I'm Sexy");
+		out.flush();
+		out.close();
+	}
+}
