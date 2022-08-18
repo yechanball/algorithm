c9886b53ab1f9c5923778fc0495d186ab3396958
Yechan <rhddpcks@gmail.com>
2022-08-18 18:03:51 +0900

    refact: Refactor BOJ-3109-Java

--- baekjoon/Main_3109_빵집.java
+++ baekjoon/Main_3109_빵집.java
@@ -9,8 +9,9 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_3109_빵집 {
-	static int R, C, nowLine; // 행, 열 크기
-	static boolean[][] checkVisited; //
+	static int R, C; // 행, 열 크기
+	static boolean[][] visited; // 방문 확인 배열
+	static boolean[][] checkVisited; // 방문 확인된 배열
 	static boolean isPipeline; // 파이프라인 완성되었는지 확인
 	static int maxPipeline = Integer.MIN_VALUE; // 파이프라인의 최대 개수
 	// 탐색 방향 0:좌상, 1:좌, 2:좌하
@@ -25,8 +26,8 @@ public class Main_3109_빵집 {
 		C = Integer.parseInt(st.nextToken());
 		
 		char[][] map = new char[R][C]; // 근처 모습을 저장할 배열
-		boolean[][] visited = new boolean[R][C]; // 방문 기록 배열
-		checkVisited = new boolean[R][C]; // 방문 기록 배열
+		visited = new boolean[R][C];
+		checkVisited = new boolean[R][C];
 		for (int i = 0; i < R; i++) {
 			String input = in.readLine();
 			for (int j = 0; j < C; j++) {
@@ -35,43 +36,34 @@ public class Main_3109_빵집 {
 			}
 		}
 		
-		copyVisit(visited, checkVisited);
-		
-		setPipeline(0, 0, map, visited); // 파이프라인 찾기
+		setPipeline(0); // 파이프라인 찾기
 		
 		out.write(Integer.toString(maxPipeline));
 		out.flush();
 		out.close();
 	}
 	
-	public static void setPipeline(int start, int pipeline, char[][] map, boolean[][] originVisited) { // 여러 빵집 조합에 따른 파이프라인 수 찾기 메서드
-		if(start == R) { // 모든 빵집을 탐색 완료한 경우
-			maxPipeline = (pipeline > maxPipeline) ? pipeline : maxPipeline; // 파이프라인 최댓값 비교
-			return;
-		}else {
-			boolean[][] visited = new boolean[R][C];
-			copyVisit(originVisited, visited); // 방문 배열 초기화
-			
-			for (int i = start; i < R; i++) {
-				isPipeline = false;
-				nowLine = 0;
-				findLine(0, start, C-1, visited); // 가스관까지 라인 탐색
-				copyVisit(checkVisited, visited); // 탐색한 배열 결과 복사
-//				System.out.println("---"+i+"에서 출발! -> 개수: "+(pipeline+nowLine));
-//				printVisit(visited);
-				setPipeline(i+1, pipeline + nowLine, map, visited); // 다음 파이프라인 탐색 -> 재귀 호출
-				copyVisit(originVisited, visited); // 방문 배열 초기화
+	public static void setPipeline(int pipeline) { // 파이프라인 수 찾기 메서드
+		for (int i = 0; i < R; i++) {
+			isPipeline = false;
+			findLine(0, i, C-1); // 가스관까지 라인 탐색
+			if(isPipeline) {
+				copyVisit(checkVisited, visited); // 완성된 경로로 교체
+				pipeline++;
 			}
+//			System.out.println("---"+i+"에서 출발! -> 개수: "+pipeline);
+//			printVisit();
 		}
+		maxPipeline = (pipeline > maxPipeline) ? pipeline : maxPipeline; // 파이프라인 최댓값 비교
+		return;
 	}
 	
-	public static void findLine(int cnt, int row, int col, boolean[][] visited) { // 선택된 빵집에서 가스관까지 연결하는 메서드
+	public static void findLine(int cnt, int row, int col) { // 선택된 빵집에서 가스관까지 연결하는 메서드
 		if(isPipeline) { // 이미 파이프라인을 완성한 경우 종료
 			return;
 		}else if(cnt == C-1) {
 			isPipeline = true;
-			copyVisit(visited, checkVisited); // 탐색한 배열 결과 복사
-			nowLine = 1;
+			copyVisit(visited, checkVisited); // 완성된 경로 백업
 			return;
 		}else {
 			for (int d = 0; d < 3; d++) { // 3가지 방향 탐색
@@ -83,7 +75,7 @@ public class Main_3109_빵집 {
 					continue; // 범위를 벗어나거나 이미 방문한 곳이면 재귀 종료
 				else { // 방문 처리하고 다음 탐색 시작
 					visited[nr][nc] = true;
-					findLine(cnt+1, nr, nc, visited);
+					findLine(cnt+1, nr, nc);
 					visited[nr][nc] = false;
 				}
 			}
@@ -96,7 +88,7 @@ public class Main_3109_빵집 {
 		}
 	}
 	
-//	public static void printVisit(boolean[][] visited) { // 방문 배열 출력 메서드
+//	public static void printVisit() { // 방문 배열 출력 메서드
 //		System.out.println("------------------");
 //		for (int i = 0; i < R; i++) {
 //			for (int j = 0; j < C; j++) {
