e158c272de0985bc48d9a7f99de2f24316e3e999
Yechan <rhddpcks@gmail.com>
2022-08-19 14:28:54 +0900

    refactor: Refactor BOJ-1987-Java

--- baekjoon/Main_1987_알파벳.java
+++ baekjoon/Main_1987_알파벳.java
@@ -12,7 +12,6 @@ public class Main_1987_알파벳 {
 	static int R, C; // 행, 열 크기
 	static int maxMove = 0; // 이동할 수 있는 최대 칸 수
 	static char[][] board; // 보드
-	static HashSet<Character> set = new HashSet<Character>(); // 알파벳 중복 사용 확인용 해시셋
 	// 탐색방향 => 상, 좌, 하, 우
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, -1, 0, 1};
@@ -32,27 +31,26 @@ public class Main_1987_알파벳 {
 			}
 		}
 		
-		findMove(0, 0, 0); // 최장 경로 찾기, (row, col, depth)
+		findMove(0, 0, 0, 0); // 최장 경로 찾기, (row, col, depth)
 		
 		out.write(Integer.toString(maxMove));
 		out.flush();
 		out.close();
 	}
 	
-	public static void findMove(int row, int col, int depth) {
+	public static void findMove(int row, int col, int depth, int flag) {
 		// 범위를 벗어난 경우
 		if(row < 0 || row >= R || col < 0 || col >= C) 
 			return;
+		int idx = (int)(board[row][col]-'A');
 		//  이미 지나간 알파벳인 경우 -> 최댓값 비교
-		if(set.contains(board[row][col])) {
+		if((flag & 1<<idx) != 0) {
 			maxMove = (depth > maxMove) ? depth : maxMove;
 			return;
 		}
 		
-		set.add(board[row][col]); // 사용한 문자 체크
 		for (int d = 0; d < 4; d++) { // 현재 위치와 인접한 노드 검사
-			findMove(row + dr[d], col + dc[d], depth+1); // 재귀 호출
+			findMove(row + dr[d], col + dc[d], depth+1, flag | 1<<idx); // 재귀 호출
 		}
-		set.remove(board[row][col]); // 사용한 문자 해제
 	}
 }
