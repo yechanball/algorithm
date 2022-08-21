6112a4b55ce4592edd33893e67947ec9a61eae99
Yechan <rhddpcks@gmail.com>
2022-08-21 16:51:58 +0900

    refact: Refactor BOJ-2609-Java

--- baekjoon/Main_2609_최대공약수와최소공배수.java
+++ baekjoon/Main_2609_최대공약수와최소공배수.java
@@ -1,24 +1,31 @@
 package algorithm.baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main_2609_최대공약수와최소공배수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 		int num1 = Integer.parseInt(st.nextToken());
 		int num2 = Integer.parseInt(st.nextToken());
-		int gcd = 1;
-		int lcm = 0;
-		
-		for (int i = 1; i <= ((num1>num2)?num2:num1); i++) {
-			if(num1%i == 0 && num2%i == 0) gcd = i;
-		}
-		lcm = num1 * num2 / gcd;
-		System.out.println(gcd);
-		System.out.print(lcm);		
+		int gcdNum = (num1 > num2) ? gcd(num1, num2) : gcd(num2, num1);
+		sb.append(gcdNum).append("\n").append(num1 * num2 / gcdNum);
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+	
+	public static int gcd(int a, int b) { // 유클리드 호제법으로 최대공약수 계산
+		if(a%b == 0)
+			return b;
+		else
+			return gcd(b, a%b);
 	}
 }
