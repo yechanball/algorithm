12f0a3cd0b1867092d1ad19e94eaf2a28a94d3f0
Yechan <rhddpcks@gmail.com>
2022-08-21 23:28:22 +0900

    refact: Refactor BOJ-10422-Java

--- baekjoon/Main_10422_괄호.java
+++ baekjoon/Main_10422_괄호.java
@@ -7,35 +7,30 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main_10422_괄호 {
-	static int L; // 괄호 문자열 길이
-	static long bracketCnt; // 올바른 괄호 문자열의 개수
-	
-	public static void brackets(int cnt, int sum) {
-		if(sum < 0) return; // 괄호)가 더 들어간 경우
-		else if(cnt == L) { // 문자열을 괄호로 다 채운 경우
-			if(sum == 0) { // 괄호가 맞게 닫힌 경우
-				bracketCnt++;
-				return;
-			}else return; // 괄호가 맞게 안 닫힌 경우
-		}else {
-			brackets(cnt+1, sum+1); // 괄호(를 여는 경우
-			brackets(cnt+1, sum-1); // 괄호)를 닫는 경우
-		}
-		
-	}
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
+		
+		long[] answer = new long[5001]; // 1 <= 문자열의 길이 <= 5000
+		answer[0] = 1;
+		answer[2] = 1;
+		for (int i = 2; i <= 2500; i++) { // L이 4부터 5000까지 짝수만 고려 -> 문자열 길이에 따른 문자열 개수 배열 생성
+			int len = i * 2; // 길이가 len일때
+			int subLen1 = i * 2 - 2; // subLen1 + subLen2 = len - 2
+			int subLen2 = 0;
+			while(subLen1 >= 0) {
+				answer[len] += (answer[subLen1] * answer[subLen2]);
+				answer[len] %= 1_000_000_007; // 개수를 1,000,000,007로 나눈 나머지로 저장
+				subLen1 -= 2;
+				subLen2 += 2;
+			}
+		}
+		
 		int T = Integer.parseInt(in.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			L = Integer.parseInt(in.readLine());
-			bracketCnt = 0; // 초기화
-			if(L % 2 == 0) { // 짝수인 경우만 고려				
-				brackets(0, 0); // 괄호 문자열 생성 호출
-			}
-			sb.append(bracketCnt).append("\n"); // 생성 가능한 괄호 문자열 개수 출력
+			int L = Integer.parseInt(in.readLine());
+			sb.append(answer[L]).append("\n"); // 생성 가능한 괄호 문자열 개수 출력
 		}
 		out.write(sb.toString());
 		out.flush();
