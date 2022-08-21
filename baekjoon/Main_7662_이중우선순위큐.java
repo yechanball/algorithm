4d3a896528ac8c9cc5252d9aaf1dc306a3100ca5
Yechan <rhddpcks@gmail.com>
2022-08-22 03:35:59 +0900

    refact: Refactor BOJ-7662-Java

--- baekjoon/Main_7662_이중우선순위큐.java
+++ baekjoon/Main_7662_이중우선순위큐.java
@@ -5,9 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main_7662_이중우선순위큐 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -18,21 +17,12 @@ public class Main_7662_이중우선순위큐 {
 		
 		int T = Integer.parseInt(in.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			PriorityQueue<Integer> minQ = new PriorityQueue<>(new Comparator<Integer>() {
-				@Override
-				public int compare(Integer n1, Integer n2) { // 오름차순 정렬
-					return n1 - n2;
-				}
-			});
-			
-			PriorityQueue<Integer> maxQ = new PriorityQueue<>(new Comparator<Integer>() {
-				@Override
-				public int compare(Integer n1, Integer n2) { // 내림차순 정렬
-					return n2 - n1;
-				}
-			});
-			
-			int size = 0; // 큐 사이즈
+			// TreeMap은 key를 기준으로 정렬된 상태 유지
+			// TrreMap -> key : 주어진 숫자, value: 숫자 개수
+			// getOrDefault(Object key, V DefaultValue)
+			// 찾는 key가 존재하면 해당 key에 매핑되어 있는 값을 반환, 그렇지 않으면 DefaultValue이 반환.
+			TreeMap<Integer, Integer> tMap = new TreeMap<>(); 
+
 			int N = Integer.parseInt(in.readLine());
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(in.readLine(), " ");
@@ -40,19 +30,27 @@ public class Main_7662_이중우선순위큐 {
 				int num = Integer.parseInt(st.nextToken());
 				
 				if(input == 'D') { // D 연산인 경우 삭제
-					if(size != 0) {
-						if(num == 1) maxQ.poll(); // D 1 : 최댓값 제거
-						else minQ.poll(); // D -1 : 최솟값 제거
-						size--;
+					if(!tMap.isEmpty()) {
+						if(num == 1) { // D 1 : 최댓값 제거
+							int maxKey = tMap.lastKey();
+							if(tMap.get(maxKey) == 1) // 최댓값이 하나밖에 없다면 제거
+								tMap.remove(maxKey);
+							else // 최댓값이 한개보다 많다면 개수를 하나 줄이기
+								tMap.put(maxKey, tMap.get(maxKey) - 1);
+						}
+						else { // D -1 : 최솟값 제거
+							int minKey = tMap.firstKey();
+							if(tMap.get(minKey) == 1) // 최솟값이 하나밖에 없다면 제거
+								tMap.remove(minKey);
+							else // 최솟값이 한개보다 많다면 개수를 하나 줄이기
+								tMap.put(minKey, tMap.get(minKey) - 1);
+						}
 					}
 				}else { // I 연산인 경우 삽입
-					minQ.offer(num);
-					maxQ.offer(num);
-					size++;
-				}
+					tMap.put(num, tMap.getOrDefault(num, 0) + 1);				}
 			}
-			if(size == 0) sb.append("EMPTY").append("\n"); // 비어있는 경우
-			else sb.append(maxQ.poll()).append(" ").append(minQ.poll()).append("\n"); // 최댓값 최솟값 출력
+			if(tMap.isEmpty()) sb.append("EMPTY").append("\n"); // 비어있는 경우
+			else sb.append(tMap.lastKey()).append(" ").append(tMap.firstKey()).append("\n"); // 최댓값 최솟값 출력
 		}
 		out.write(sb.toString());
 		out.flush();
