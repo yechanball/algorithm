7ed0631013f3d6448da6f0506ae014b2d36601d5
Yechan <rhddpcks@gmail.com>
2022-08-23 09:37:52 +0900

    refactor: Refactor BOJ-7576-Java

--- baekjoon/Main_7576_토마토.java
+++ baekjoon/Main_7576_토마토.java
@@ -14,16 +14,16 @@ import java.util.StringTokenizer;
 public class Main_7576_토마토 {
 	static int N, M; // N: 행, M: 열
 	static int[][] storage; // 토마토 보관 창고
-	static List<int[]> tomato; // 초기에 주어진 익은 토마토 정보를 저장할 리스트
+	static List<int[]> tomato = new ArrayList<int[]>(); // 초기에 주어진 익은 토마토 정보를 저장할 리스트
 	static int[][] visited; // 토마토 방문 기록 -> -1: 방문할 수 없는 곳, 0: 방문하지 않은 곳, 1 ~ 숫자: 방문하는데 걸린 시간
-	static int minTime;
+	static int minTime = Integer.MIN_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		
 		storage = new int[N][M]; // 토마토 창고 정보 입력
 		for (int i = 0; i < N; i++) {
@@ -36,46 +36,55 @@ public class Main_7576_토마토 {
 			}
 		}
 		
-		int from, to;
-		st = new StringTokenizer(in.readLine(), " ");
-		for (int i = 0; i < L/2; i++) {
-			from = Integer.parseInt(st.nextToken());
-			to = Integer.parseInt(st.nextToken());
-			nodes[from].add(to); // from의 인접 노드 to 추가
+		visited = new int[N][M];
+		bfs(); // BFS 탐색
+
+		boolean isWellDone = true;
+		for (int i = 0; i < N; i++) { // 토마토가 전부 익었는지 확인
+			for (int j = 0; j < M; j++) {
+				if(storage[i][j] == 0) { // 토마토가 익지 않았다면 시간을 -1로 지정
+					isWellDone = false;
+					break;
+				}
+				if(visited[i][j] > minTime) {
+					minTime = visited[i][j]-1;
+				}
+			}
 		}
 		
-		visited = new boolean[101];
-		maxTime = Integer.MIN_VALUE; // 초기화
-		maxNode = Integer.MIN_VALUE;
-		
-		bfs(start);
-
-		out.write();
+		if(!isWellDone) minTime = -1;
+		out.write(Integer.toString(minTime));
 		out.flush();
 		out.close();
 	}
 	
-	public static void bfs(int startNode) {
-		Deque<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {startNode, 0}); // 0: 노드 번호, 1: 소요 시간
+	public static void bfs() {
+		int[] dr = {-1,1,0,0}; // 상하좌우
+		int[] dc = {0,0,-1,1};
 		
-		while(!q.isEmpty()) {
+		Deque<int[]> q = new ArrayDeque<int[]>();
+		
+		for (int i = 0, size = tomato.size(); i < size; i++) { // 초기에 익은 토마토가 주어질때 큐에 우선적으로 넣기
+			q.offer(new int[] {tomato.get(i)[0], tomato.get(i)[1], 1}); // (0, 1): 좌표, 2: 소요 시간
+			visited[tomato.get(i)[0]][tomato.get(i)[1]] = 1;
+		}
+		
+		while(!q.isEmpty()) {;
 			int[] now = q.poll();
-			visited[now[0]] = true;
+			storage[now[0]][now[1]] = 1;
+			visited[now[0]][now[1]] = now[2];
 			
-			for (int i = 0, size = nodes[now[0]].size(); i < size; i++) { // 인접 노드 탐색
-				int nextNode = nodes[now[0]].get(i);
-				if(!visited[nextNode]) {
-					q.offer(new int[] {nextNode, now[1]+1});
-					visited[nextNode] = true;
+			for (int i = 0; i < 4; i++) { // 인접 노드 탐색
+				int nr = now[0]+dr[i];
+				int nc = now[1]+dc[i];
+				
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M || storage[nr][nc] == -1)
+					continue;
+				
+				if(visited[nr][nc] == 0) {
+					q.offer(new int[] {nr, nc, now[2]+1});
+					visited[nr][nc] = now[2]+1;
 				}
-			}
-			
-			if(now[1] > maxTime) { // 현재 노드가 최대 시간 걸린 노드라면 갱신 
-				maxTime = now[1];
-				maxNode = now[0];
-			}else if(now[1] == maxTime) { // 현재 노드가 최대 시간 걸린 노드와 동일한 경우 노드 번호가 큰 것으로 갱신
-				maxNode = (now[0] > maxNode) ? now[0] : maxNode;
 			}
 		}
 	}
