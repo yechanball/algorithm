d80900af67ee134aff318c6ba12931ae763b5fab
Yechan <rhddpcks@gmail.com>
2022-10-02 00:15:58 +0900

    refactor: Refactor SWEA-1249-Java

--- swea/Solution_1249_보급로.java
+++ swea/Solution_1249_보급로.java
@@ -9,9 +9,9 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Solution_1249_보급로 {
-	static int N, minTime = Integer.MAX_VALUE;
+	static int N;
 	static int[][] map;
-	static int[][] visited;
+	static int[][] time;
 	static int[] dr = new int[] {-1, 1, 0, 0};
 	static int[] dc = new int[] {0, 0, -1, 1};
 	
@@ -24,55 +24,46 @@ public class Solution_1249_보급로 {
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(in.readLine());
 			map = new int[N][N];
-			visited = new int[N][N];
+			time = new int[N][N];
 			
 			for (int i = 0; i < N; i++) {
 				String input = in.readLine();
 				for (int j = 0; j < N; j++) {
 					map[i][j] = input.charAt(j) - '0';
+					time[i][j] = Integer.MAX_VALUE;
 				}
 			}
+
+			bfs(0, 0); // 너비 우선 탐색
 			
-			minTime = Integer.MAX_VALUE;
-			bfs(0, 0, 0);
-			sb.append("#").append(tc).append(" ").append(minTime).append("\n");
+			sb.append("#").append(tc).append(" ").append(time[N-1][N-1]).append("\n");
 		}
 		out.write(sb.toString());
 		out.flush();
 		out.close();
 	}
 	
-	public static void bfs(int row, int col, int t) {
+	public static void bfs(int row, int col) {
 		Deque<int[]> q = new ArrayDeque<int[]>();
-		q.offer(new int[] {row, col, t});
+		q.offer(new int[] {row, col});
+		time[row][col] = 0; // 출발지 시간 초기화
 		
 		while(!q.isEmpty()) {
 			int[] curr = q.poll();
-			int r = curr[0], c = curr[1], time = curr[2], nr, nc;
-			
-			if(r == N-1 && c == N-1) { // 도착한 경우 최소시간 확인
-				minTime = (time < minTime) ? time : minTime;
-				continue;
-			}
-			
-			if(visited[r][c] != 0) {
-				visited[r][c] = (time < visited[r][c]) ? time : visited[r][c];
-			}else {
-				visited[r][c] = time;				
-			}
-			
-			if(time > minTime) { // 현재 누적 시간이 최소시간보다 긴 경우 종료
-				continue;
-			}
+			int r = curr[0], c = curr[1], nr, nc;
 			
 			for (int i = 0; i < 4; i++) { // 인접 탐색
 				nr = r + dr[i];
 				nc = c + dc[i];
 				
-				if(nr < 0 || nr >= N || nc < 0 || nc >= N) {
+				if(nr < 0 || nr >= N || nc < 0 || nc >= N) { // 지도를 벗어나는 경우
 					continue;
 				}
-				q.offer(new int[] {nr, nc, time+map[nr][nc]});
+				
+				if(time[nr][nc] > time[r][c] + map[nr][nc]) { // 다음 장소에 갈 수 있는 시간이 현재 위치를 거치는 것이 더 빠른 경우
+					time[nr][nc] = time[r][c] + map[nr][nc]; // 최소 시간 갱신
+					q.offer(new int[] {nr, nc});
+				}
 			}
 		}
 	}
