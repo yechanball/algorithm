30522673b479ef0e19f0d59b0d54979a6ca43578
Yechan <rhddpcks@gmail.com>
2022-10-11 23:25:45 +0900

    feat: Add SWEA-4014-Java

---
+++ swea/Solution_4014_활주로건설.java
@@ -0,0 +1,96 @@
+package algorithm.swea;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution_4014_활주로건설 {
+	static int N, X, road, preTop, cnt;
+	static int[][] map;
+	static boolean isDown, canMake;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(in.readLine());		
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			X = Integer.parseInt(st.nextToken());
+			
+			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(in.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			road = 0;
+			for (int r = 0; r < N; r++) { // 가로 확인
+				preTop = map[r][0];
+				cnt = 1;
+				isDown = false;
+				canMake = true;
+				
+				for (int c = 1; c < N; c++) {
+					canMakeRoad(r, c);
+					if(!canMake) break;
+				}
+				
+				if(isDown && cnt < X) canMake = false;
+				
+				if(canMake) road++; // 만들 수 있는 경우 활주로 개수 증가 
+			}
+			
+			for (int c = 0; c < N; c++) { // 세로 확인
+				preTop = map[0][c];
+				cnt = 1;
+				isDown = false;
+				canMake = true;
+				
+				for (int r = 1; r < N; r++) {
+					canMakeRoad(r, c);
+					if(!canMake) break;
+				}
+				
+				if(isDown && cnt < X) canMake = false;
+				
+				if(canMake) road++; // 만들 수 있는 경우 활주로 개수 증가 
+			}	
+			sb.append("#").append(tc).append(" ").append(road).append("\n");
+		}
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+	
+	public static void canMakeRoad(int r, int c) {
+		if(map[r][c] == preTop) { // 높이가 같은 경우 
+			cnt++;
+		}
+		else if(Math.abs(preTop - map[r][c]) > 1) { // 높이 차이가 1 초과인 경우 불가능
+			canMake = false;				
+		}
+		else if(map[r][c] < preTop) { // 높이가 낮은 경우 
+			if(isDown && cnt < X) canMake = false;
+	
+			preTop = map[r][c];
+			cnt = 1;
+			isDown = true;
+		}
+		else if(map[r][c] > preTop) { // 높이가 높아진 경우 
+			if((isDown && cnt < X*2) || cnt < X) canMake = false;
+			
+			preTop = map[r][c];
+			cnt = 1;
+			isDown = false;
+		}
+	}
+}
