682fe04dfaf55a87018e0b0bf002f76cb551af27
Yechan <rhddpcks@gmail.com>
2022-10-12 00:22:51 +0900

    feat: Add BOJ-2749-Java

---
+++ baekjoon/Main_2749_피보나치수3.java
@@ -0,0 +1,50 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2749_피보나치수3 {
+	static int M = 2; // 행렬의 크기
+	static long C = 1_000_000; // 나눠야할 수
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		long N = Long.parseLong(in.readLine()); // N번째 피보나치 수	
+		long[][] A = new long[][] {{1, 1}, {1, 0}}; // 피보나치 수를 구하기 위한 행렬
+		long[][] result = squared(A, N-1); // N번째 피보나치 수는 N-1 제곱하였을 때 [0, 0] 값
+		
+		System.out.print(result[0][0]);
+	}
+	
+	public static long[][] squared(long[][] A, long B) { // 분할정복 거듭제곱
+		if(B == 1 || B == 0) return remain(A);
+		long[][] half = squared(A, B/2);
+		return (B%2 == 0) ? remain(multiple(half, half)) : remain(multiple(multiple(half, half), A));
+	}
+	
+	public static long[][] multiple(long[][] m1, long[][] m2){ // 행렬 곱
+		long[][] result = new long[M][M];
+		
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				for (int k = 0; k < M; k++) {
+					result[i][j] += m1[i][k] * m2[k][j];					
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public static long[][] remain(long[][] m){ // 나머지 연산
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				m[i][j] %= C;
+			}
+		}
+		
+		return m;
+	}
+}
