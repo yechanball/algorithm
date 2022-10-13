f10688502e5b6eb2f82da058b356cb3fa35a601d
Yechan <rhddpcks@gmail.com>
2022-10-13 18:54:37 +0900

    refactor: Refactor SWEA-3238-Java

--- swea/Solution_3238_이항계수구하기.java
+++ swea/Solution_3238_이항계수구하기.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution_3238_이항계수구하기 {
-	static long P; // 나눠야할 수
+	static int P; 
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -21,36 +21,37 @@ public class Solution_3238_이항계수구하기 {
 			st = new StringTokenizer(in.readLine(), " ");
 			long N = Long.parseLong(st.nextToken());
 			long R = Long.parseLong(st.nextToken());
-			P = Long.parseLong(st.nextToken());
-			long D = N - R;
+			int P = Integer.parseInt(st.nextToken());
 			
-			long factN, factR = 1, factD = 1;
-			
-			long tmp = 1;
-			for (long i = 1; i <= N; i++) {
-				tmp = tmp * i % P;
-				if(i == R) factR = tmp;
-				if(i == D) factD = tmp;
+			long[] fact = new long[P+1];
+			fact[0] = 1;
+			for (int i = 1; i <= P; i++) {
+				fact[i] = i * fact[i-1] % P;
 			}
-			factN = tmp;
 			
-			// 역원 값 찾기 (R!)^(-1) mod P = (R!)^(P-2)
-			long inverseR = pow(factR, P-2);
-			long inverseD = pow(factD, P-2);
-			
-			// nCr = n! * r^(-1) * (n-r)^(-1)
-			long answer = factN * (inverseR * inverseD % P) % P; 
+			long answer = 1;
+	        while (R > 0 || N > 0){
+	            int X = (int) (N % P); 
+	            int Y = (int) (R % P);
+
+	            if (Y > X){
+	                answer = 0;
+	                break;
+	            }
+
+	            answer = answer * fact[X] % P;
+	            for (int i = 0; i < P - 2; i++){
+	                answer = answer * fact[X - Y] * fact[Y] % P;
+	            }
+	            N /= P; 
+	            R /= P;
+	        }
+	        answer %= P;
 			
 			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
 		out.write(sb.toString());
 		out.flush();
 		out.close();
-	}
-
-	private static long pow(long A, long X) { // A^X 거듭제곱 계산 -> 분할정복
-		if(X == 0) return 1;
-		long half = pow(A, X/2) % P;
-		return (X%2 == 0) ? half*half%P : (half*half%P)*A%P;
 	}
 }
