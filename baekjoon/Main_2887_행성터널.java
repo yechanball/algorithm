1af5b9eacda11cca4598f82713fc43f84b005fa1
Yechan <rhddpcks@gmail.com>
2022-10-23 22:09:27 +0900

    refactor: Refactor BOJ-2887-Java

--- baekjoon/Main_2887_행성터널.java
+++ baekjoon/Main_2887_행성터널.java
@@ -1,11 +1,11 @@
-// 메모리 초과 -> 메모리 사용을 줄일 방법을 생각해보자.
-
 package algorithm.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -13,7 +13,23 @@ import java.util.StringTokenizer;
 public class Main_2887_행성터널 {
 	static int V; // 정점 개수
 	static int[] parents; // 각 정점의 부모 저장 배열
-	static List<Integer> planet; // 행성 좌표 배열
+	static List<Planet> planetList; // 행성 좌표 배열
+	static PriorityQueue<Edge> edgeList = new PriorityQueue<Edge>(); // 간선 가중치 우선순위 큐
+		
+	static class Planet { // 행성 좌표 정보
+		int no; // 행성 번호
+		int x; // X좌표
+		int y; // Y좌표
+		int z; // Z좌표
+		
+		public Planet(int no, int x, int y, int z) {
+			super();
+			this.no = no;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
 	
 	static class Edge implements Comparable<Edge> { // 간선 정보, 우선순위 큐를 위해 Comparable 사용
 		int from; // 시작 정점
@@ -38,23 +54,17 @@ public class Main_2887_행성터널 {
 		
 		V = Integer.parseInt(in.readLine()); // 정점 개수 입력
 		
-		planet = new ArrayList<Integer>(); // 정점 좌표 저장할 리스트 생성
+		planetList = new ArrayList<Planet>(); // 정점 좌표 저장할 리스트 생성
 		for (int i = 0; i < V; i++) { // 정점 좌표 정보 입력
 			st = new StringTokenizer(in.readLine(), " ");
-			planet.add(Integer.parseInt(st.nextToken())); // X좌표
-			planet.add(Integer.parseInt(st.nextToken())); // Y좌표
-			planet.add(Integer.parseInt(st.nextToken())); // Z좌표
+			planetList.add(new Planet(i+1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))); 
 		}
 		
 		make(); // 정점 배열 생성
 		
-		PriorityQueue<Edge> edgeList = new PriorityQueue<Edge>(); // 간선정보를 담을 작은 가중치 우선순위 큐 생성
-		for (int i = 0; i < V; i++) { // 두 정점끼리 거리를 계산하여 간선 정보 생성 및 저장
-			for (int j = i+1; j < V; j++) {
-				edgeList.add(new Edge(i+1, j+1, calcDistance(i, j)));
-			}
-		}
+		makeEdge(); // 행성 거리 계산하여 간선 정보 생성
 		
+		// 크루스칼 알고리즘
 		int edgeCnt = 0; // 간선 개수
 		int spWeightSum = 0; // 모든 간선의 가중치 합
 		while(!edgeList.isEmpty()) {  // 가중치가 낮은 간선부터 정점 연결
@@ -70,7 +80,7 @@ public class Main_2887_행성터널 {
 		
 		System.out.print(spWeightSum);
 	}
-	
+
 	static void make() { // 크기가 1인 서로소 집합 생성
 		parents = new int[V+1];
 		for (int i = 1; i <= V; i++) {
@@ -94,17 +104,38 @@ public class Main_2887_행성터널 {
 		return true;
 	}
 	
-	static int calcDistance(int num1, int num2) { // 두 정점사이 거리 계산
-		int idx1 = num1*3;
-		int x1 = planet.get(idx1);
-		int y1 = planet.get(idx1+1);
-		int z1 = planet.get(idx1+2);
+	private static void makeEdge() { // 두 정점 사이의 간선 정보 생성 및 저장
+		// X 좌표 기준 정렬
+		Collections.sort(planetList, new Comparator<Planet>() {
+			@Override
+			public int compare(Planet p1, Planet p2) {
+				return p1.x - p2.x;
+			}
+		});
+		for (int i = 0; i < V-1; i++) {
+			edgeList.add(new Edge(planetList.get(i).no, planetList.get(i+1).no, planetList.get(i+1).x - planetList.get(i).x));
+		}
 		
-		int idx2 = num2*3;
-		int x2 = planet.get(idx2);
-		int y2 = planet.get(idx2+1);
-		int z2 = planet.get(idx2+2);
+		// Y 좌표 기준 정렬
+		Collections.sort(planetList, new Comparator<Planet>() {
+			@Override
+			public int compare(Planet p1, Planet p2) {
+				return p1.y - p2.y;
+			}
+		});	
+		for (int i = 0; i < V-1; i++) {
+			edgeList.add(new Edge(planetList.get(i).no, planetList.get(i+1).no, planetList.get(i+1).y - planetList.get(i).y));
+		}
 		
-		return Math.min(Math.min(Math.abs(x1-x2), Math.abs(y1-y2)), Math.abs(z1-z2));
+		// Z 좌표 기준 정렬
+		Collections.sort(planetList, new Comparator<Planet>() {
+			@Override
+			public int compare(Planet p1, Planet p2) {
+				return p1.z - p2.z;
+			}
+		});	
+		for (int i = 0; i < V-1; i++) {
+			edgeList.add(new Edge(planetList.get(i).no, planetList.get(i+1).no, planetList.get(i+1).z - planetList.get(i).z));
+		}
 	}
 }
