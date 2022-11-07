c7a536f227a7a1f59c63e71c4cc4d8be5777806c
Yechan <rhddpcks@gmail.com>
2022-11-07 12:20:51 +0900

    refactor: Refactor BOJ-16120-Java

--- baekjoon/Main_16120_PPAP.java
+++ baekjoon/Main_16120_PPAP.java
@@ -3,23 +3,34 @@ package algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main_16120_PPAP {
 	public static void main(String[] args) throws IOException {
 		String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
-		boolean isPPAP = true;
-		if((str.length() - 1) % 3 != 0) { // 길이 조건 확인, PPAP 문자열의 길이: 첫째항이 1이고, 공차가 3인 등차수열
-			isPPAP = false;
-		}else if(!(str.length() == 1 && str.equals("P"))) { // 문자열 크기가 1이고 P인 경우를 제외한 경우 탐색
-			while(true) { // 순차적으로 변환하며 PPAP 문자열 확인
-				if(!str.contains("PPAP")) { // 문자열에 PPAP가 포함되지 않은 경우 -> false, 종료
+		Stack<Character> stack = new Stack<>();
+		boolean isPPAP = true; // PPAP 문자열 체크
+		int len = str.length(); // 문자열 길이
+		int idx = 0, size = 0; // 인덱스, 스택 크기
+		
+		while(idx < len) {
+			if(str.charAt(idx) == 'P') {
+				stack.push('P');
+				size++;
+				idx++;
+			}else {
+				if(size >= 2 && idx+1 < len && str.charAt(idx+1) == 'P') { // PPAP 부분문자열을 P로 치환
+					stack.pop();
+					size--;
+					idx += 2;
+				}else { // A문자 앞에 PP가 없는 경우 PPAP 문자열 성립 X
 					isPPAP = false;
 					break;
 				}
-				if(str.equals("PPAP")) break; // 문자열이 PPAP와 일치하는 경우 -> true, 종료
-				str = str.replaceAll("PPAP", "P"); // PPAP 부분문자열을 P로 치환
 			}
 		}
+		if(size != 1 || stack.peek() != 'P') isPPAP = false; // 문자열 길이가 1이고 P인 문자열이 아니면 PPAP 문자열 성립 X
+		
 		System.out.print((isPPAP) ? "PPAP" : "NP");
 	}
 }
