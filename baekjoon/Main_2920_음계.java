9fb7191f913b112ec5f82785ed2508a4bc68eaf7
Yechan <rhddpcks@gmail.com>
2022-11-20 23:52:43 +0900

    refactor: Refactor BOJ-2920-Java

--- baekjoon/Main_2920_음계.java
+++ baekjoon/Main_2920_음계.java
@@ -1,32 +1,10 @@
 package algorithm.baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main_2920_음계 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-		boolean check = true;
-		int start = Integer.parseInt(st.nextToken());
-		if(start == 1) {
-			for (int i = 2; i <= 8; i++) {
-				if(i == Integer.parseInt(st.nextToken())) continue;
-				check = false;
-				break;
-			}
-			System.out.print((check) ? "ascending" : "mixed");
-		}else if(start == 8) {
-			for (int i = 7; i >= 1; i--) {
-				if(i == Integer.parseInt(st.nextToken())) continue;
-				check = false;
-				break;
-			}
-			System.out.print((check) ? "descending" : "mixed");			
-		}else {
-			System.out.print("mixed");
-		}
+		int hashVal = new BufferedReader(new InputStreamReader(System.in)).readLine().hashCode();
+		System.out.print((hashVal == 1956384708) ? "ascending" : ((hashVal == -1341322556) ? "descending": "mixed"));
 	}
 }
