3a24cca7988226289e2df6bf82b3014a29c85411
Yechan <rhddpcks@gmail.com>
2022-11-23 21:52:10 +0900

    feat: Add BOJ-11654-Java

---
+++ baekjoon/Main_11654_아스키코드.java
@@ -0,0 +1,11 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_11654_아스키코드 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print((int)in.readLine().charAt(0));
+	}
+}
