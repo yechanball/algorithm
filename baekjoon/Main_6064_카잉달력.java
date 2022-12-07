e51798e8893ae488bb4fa9e8e579e2938e962258
Yechan <rhddpcks@gmail.com>
2022-12-07 19:58:48 +0900

    refactor: Refactor BOJ-6064-Java

--- baekjoon/Main_6064_카잉달력.java
+++ baekjoon/Main_6064_카잉달력.java
@@ -1,39 +1,29 @@
 package algorithm.baekjoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main_6064_카잉달력 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
 		int T = Integer.parseInt(in.readLine());
 		for (int tc = 0; tc < T; tc++) {
 			StringTokenizer st = new StringTokenizer(in.readLine());
 			int M = Integer.parseInt(st.nextToken());
 			int N = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+		
 			int k = -1;
-			int tmp = x;
-			while(tmp <= M*N) {
-				if(tmp%N == y || (tmp%N == 0 && N == y)) {
-					k = tmp;
+			for (int i = x, max = M*N; i < max; i += M) {
+				if(i % N == y) {
+					k = i + 1;
 					break;
 				}
-				tmp += M;
 			}
-			sb.append(k + "\n");
+			System.out.println(k);
 		}
-		out.write(sb.toString());
-		out.flush();
-		out.close();
 	}
 }
