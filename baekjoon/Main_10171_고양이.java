c5ecae52174d3999c0bdc4931c235d0180f2a365
Yechan <rhddpcks@gmail.com>
2022-12-25 17:53:59 +0900

    feat: Add BOJ-10171-Java

---
+++ baekjoon/Main_10171_고양이.java
@@ -0,0 +1,7 @@
+package algorithm.baekjoon;
+
+public class Main_10171_고양이 {
+	public static void main(String[] args) {
+		System.out.print("\\    /\\\n"+" )  ( ')\n"+"(  /  )\n"+" \\(__)|");
+	}
+}
