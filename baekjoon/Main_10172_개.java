37dd2a1ca694aad0a14ce0177f72a992e2f66805
Yechan <rhddpcks@gmail.com>
2022-12-27 18:05:42 +0900

    feat: Add BOJ-10172-Java

---
+++ baekjoon/Main_10172_개.java
@@ -0,0 +1,7 @@
+package algorithm.baekjoon;
+
+public class Main_10172_개 {
+	public static void main(String[] args) {
+		System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+	}
+}
