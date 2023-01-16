f695b26cd6e6ec1d2cb6461c167dc90dc22e4c1b
Yechan <rhddpcks@gmail.com>
2023-01-16 23:53:20 +0900

    refact: Refactor BOJ-24416-Java

--- baekjoon/Main_24416_알고리즘수업피보나치수1.java
+++ baekjoon/Main_24416_알고리즘수업피보나치수1.java
@@ -5,31 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_24416_알고리즘수업피보나치수1 {
-	static int recursionCnt = 0, dpCnt = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(in.readLine());
-		fib(n);
-		fibonacci(n);
-		System.out.print(recursionCnt + " " + dpCnt);
-	}
-	
-	private static int fib(int n) {
-		if(n == 1 || n == 2) {
-			recursionCnt++;
-			return 1;
-		}
-		else return (fib(n - 1) + fib(n - 2));
-	}
-	
-	private static int fibonacci(int n) {
-		int[] f = new int[n + 1];
-		f[1] = 1;
-		f[2] = 1;		
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 1;		
 		for (int i = 3; i <= n; i++) {
-			dpCnt++;
-			f[i] = f[i - 1] + f[i - 2];
+			dp[i] = dp[i - 1] + dp[i - 2];
 		}
-		return f[n];
+		System.out.print(dp[n] + " " + (n-2));
 	}
 }
