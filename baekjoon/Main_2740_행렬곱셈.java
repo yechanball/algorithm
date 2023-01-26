505b139f6cc3fe3391415c8a4488e84dc5d731c7
Yechan <rhddpcks@gmail.com>
2023-01-26 23:54:42 +0900

    feat: Add BOJ-2740-Java

---
+++ baekjoon/Main_2740_행렬곱셈.java
@@ -0,0 +1,49 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2740_행렬곱셈 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] A = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < M; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		st = new StringTokenizer(in.readLine());
+		M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] B = new int[M][K];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < K; j++) {
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] result = new int[N][K];
+		for(int i = 0; i < N; i++){
+            for(int j = 0; j < K; j++){
+                for(int k = 0; k < M; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < K; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
