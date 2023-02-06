9f957bcaaf61cfde3b1eb3d659433dd924403a5a
Yechan <rhddpcks@gmail.com>
2023-02-07 02:40:50 +0900

    feat: Add BOJ-2743-Java

---
+++ baekjoon/Main_2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2743_단어길이재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(in.readLine().length());
+	}
+}
