3a76a352b8ccbf9f3bde7818c439a1e41b46d5e8
Yechan <rhddpcks@gmail.com>
2023-02-24 23:13:39 +0900

    feat: Add BOJ-8394-Java

---
+++ baekjoon/Main_8394_악수.java
@@ -0,0 +1,19 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_8394_악수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		int[] dp = new int[n+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10;
+		}
+		System.out.print(dp[n]);
+	}
+}
