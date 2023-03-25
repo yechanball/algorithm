8ac810a5ac555e0e6acfdfef10b5864352c60bf3
Yechan <rhddpcks@gmail.com>
2023-03-25 21:52:04 +0900

    feat: Add BOJ-2738-Java

---
+++ baekjoon/Main_2738_행렬덧셈.java
@@ -0,0 +1,46 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_2738_행렬덧셈 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] matrix = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < M; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < M; j++) {
+				matrix[i][j] += Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				sb.append(matrix[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+}
