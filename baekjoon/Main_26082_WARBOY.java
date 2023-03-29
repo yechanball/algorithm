1fbbec7f9708b419220b500ffd383789539bf1cc
Yechan <rhddpcks@gmail.com>
2023-03-29 23:00:25 +0900

    feat: Add BOJ-26082-Java

---
+++ baekjoon/Main_26082_WARBOY.java
@@ -0,0 +1,15 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_26082_WARBOY {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
+		System.out.print(B/A*C*3);
+	}
+}
