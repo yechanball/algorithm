e1080923f0f400aa3968f728ccd391e515c6e3a6
Yechan <rhddpcks@gmail.com>
2023-03-30 23:57:40 +0900

    feat: Add BOJ-27512-Java

---
+++ baekjoon/Main_27512_스네이크.java
@@ -0,0 +1,18 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_27512_스네이크 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		if(n%2 == 0 || m%2 ==0) System.out.print(m*n);
+		else System.out.print(m*n-1);
+	}
+}
