2c4db20d3bced26cdf118a50f0f58be67b73a7a6
Yechan <rhddpcks@gmail.com>
2023-04-06 23:39:13 +0900

    refact: Refactor BOJ-1261-Java

--- baekjoon/Main_1261_알고스팟.java
+++ baekjoon/Main_1261_알고스팟.java
@@ -3,11 +3,12 @@ package algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main_1261_알고스팟 {
-	static class Room implements Comparable<Room>{
+	static class Room{
 		int r, c, cnt;
 
 		public Room(int r, int c, int cnt) {
@@ -15,15 +16,10 @@ public class Main_1261_알고스팟 {
 			this.c = c;
 			this.cnt = cnt;
 		}
-
-		@Override
-		public int compareTo(Room r) {
-			return cnt - r.cnt;
-		}	
 	}
 	
 	static int N, M;
-	static int[][] maze, cnt;
+	static int[][] maze;
 	static boolean[][] visited;
 	
 	public static void main(String[] args) throws IOException {
@@ -34,34 +30,31 @@ public class Main_1261_알고스팟 {
 		N = Integer.parseInt(st.nextToken());
 		
 		maze = new int[N+1][M+1];
-		cnt = new int[N+1][M+1];
 		for (int i = 1; i <= N; i++) {
 			String str = in.readLine();
 			for (int j = 1; j <= M; j++) {
 				maze[i][j] = str.charAt(j-1) - '0';
-				cnt[i][j] = Integer.MAX_VALUE;
 			}
 		}
 		
 		visited = new boolean[N+1][M+1];
-		dijkstra(new Room(1, 1, 0));
+		int minCnt = dijkstra(new Room(1, 1, 0));
 		
-		System.out.print(cnt[N][M]);
+		System.out.print(minCnt);
 	}
 
-	public static void dijkstra(Room start) {
+	public static int dijkstra(Room start) {
 		int[] dr = {0,1,0,-1}; // 우하좌상
 		int[] dc = {1,0,-1,0};
 		
-		PriorityQueue<Room> pq = new PriorityQueue<>();
-		pq.add(start);
-		cnt[start.r][start.c] = 0;
+		Deque<Room> que = new ArrayDeque<>();
+		que.add(start);
 		visited[start.r][start.c] = true;
 		
-		while(!pq.isEmpty()) {
-			Room curr = pq.poll();
+		while(!que.isEmpty()) {
+			Room curr = que.poll();
 			
-			if(curr.r == N && curr.c == M) return;
+			if(curr.r == N && curr.c == M) return curr.cnt;
 			
 			for (int i = 0; i < 4; i++) {
 				int nr = curr.r + dr[i];
@@ -69,14 +62,13 @@ public class Main_1261_알고스팟 {
 
 				if(nr < 1 || nr > N || nc < 1 || nc > M || visited[nr][nc]) continue;
 				
-				int nextCnt = curr.cnt + maze[nr][nc];
+				if(maze[nr][nc] == 0) que.addFirst(new Room(nr, nc, curr.cnt));
+				else que.addLast(new Room(nr, nc, curr.cnt+1));
 				
-				if(nextCnt < cnt[nr][nc]) {
-					pq.add(new Room(nr, nc, nextCnt));
-					cnt[nr][nc] = nextCnt;
-					visited[nr][nc] = true;					
-				}
+				visited[nr][nc] = true;
 			}
 		}
+		
+		return 0;
 	}
 }
