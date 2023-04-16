ba02007a8f29681a5cc0ce5a1662e329a9500edc
Yechan <rhddpcks@gmail.com>
2023-04-17 00:38:35 +0900

    feat: Add BOJ-27959-Java

---
+++ baekjoon/Main_27959_초코바.java
@@ -0,0 +1,18 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_27959_초코바 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.print((N * 100 >= M) ? "Yes" : "No");
+	}
+}
