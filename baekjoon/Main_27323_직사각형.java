17c666e8abe5bfdc4e8365d134cc5fd62439e2bf
Yechan <rhddpcks@gmail.com>
2023-05-11 22:27:42 +0900

    feat: Add BOJ-27323-Java

---
+++ baekjoon/Main_27323_직사각형.java
@@ -0,0 +1,12 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_27323_직사각형 {
+public static void main(String[] args)throws IOException {
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	System.out.print(Integer.parseInt(in.readLine())*Integer.parseInt(in.readLine()));
+}
+}
