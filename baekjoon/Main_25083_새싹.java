fe6d565dddf4f6209c73f6c298273aa048eba0e0
Yechan <rhddpcks9814@naver.com>
2023-05-27 11:49:11 +0900

    feat: Add BOJ-25083-Java

---
+++ baekjoon/Main_25083_새싹.java
@@ -0,0 +1,12 @@
+package algorithm.baekjoon;
+
+public class Main_25083_새싹 {
+	public static void main(String[] args) {
+		System.out.print("         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |");
+	}
+}
