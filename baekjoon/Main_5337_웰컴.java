dc376efd3965d2770598c87b26622c5f1fb60401
Yechan <rhddpcks9814@naver.com>
2023-06-19 00:33:22 +0900

    feat: Add BOJ-5337-Java

---
+++ baekjoon/Main_5337_웰컴.java
@@ -0,0 +1,9 @@
+package algorithm.baekjoon;
+
+public class Main_5337_웰컴 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
