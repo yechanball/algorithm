7daf01d2ef8de07478a781187ca43192ce050e96
Yechan <rhddpcks9814@naver.com>
2023-07-05 09:41:20 +0900

    feat: Add BOJ-5338-Java

---
+++ baekjoon/Main_5338_마이크로소프트로고.java
@@ -0,0 +1,11 @@
+package algorithm.baekjoon;
+
+public class Main_5338_마이크로소프트로고 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n" + 
+						   "'-..-'|   ||   |\n" + 
+						   "'-..-'|_.-;;-._|\n" + 
+						   "'-..-'|   ||   |\n" + 
+						   "'-..-'|_.-''-._|");
+	}
+}
