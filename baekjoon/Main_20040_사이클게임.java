228ecbf4e36e8cafaa7409457f4ee33bde3bc23c
Yechan <rhddpcks9814@naver.com>
2023-07-17 22:34:06 +0900

    feat: Add BOJ-20040-Java

---
+++ baekjoon/Main_20040_사이클게임.java
@@ -0,0 +1,58 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_20040_사이클게임 {
+	static int[] parents;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		make(n);
+		
+		int num = 0;
+		for (int i = 1; i <= m; i++) {
+			st = new StringTokenizer(in.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(!union(a, b)) {
+				num = i;
+				break;
+			}
+		}
+		
+		System.out.println(num);
+	}
+	
+	static void make(int n) {
+		parents = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	static int find(int a) {
+		if(parents[a] == a) return a;
+		
+		return parents[a] = find(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}
