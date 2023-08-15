e6bd3e7e95d976fd3d73a0f64fa5494599b93fe7
Yechan <rhddpcks9814@naver.com>
2023-08-16 02:12:18 +0900

    feat: Add BOJ-15894-Java

---
+++ baekjoon/Main_15894_수학은체육과목입니다.java
@@ -0,0 +1,12 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_15894_수학은체육과목입니다 {
+	public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(in.readLine());
+        System.out.println(n*4);
+	}
+}
